0c3652e982b0d54886f3e37dc63ca394c285698f
Badam Nikitha <badam.nikitha@covalensedigital.net>
2022-05-31 11:55:43 +0530

    adding jenkins pipeline script

--- jenkinfile.groovy
+++ jenkinfile.groovy
@@ -3,13 +3,13 @@ agent any
 stages{
 stage('checkout codebase'){
 steps{
-  checkout scm:[$class: 'GitSCM',userRemoteConfigs: [[crendentialsId: 'ssh-key', url: 'git@github.com/:badam-nikitha/multithreading-example-1.git']]]
+  checkout scm:[$class: 'GitSCM',userRemoteConfigs: [[crendentialsId: 'ssh-key', url: 'git@github.com:badam-nikitha/multithreading-example1.git]]]
      }
 
 }
 stage('Build'){
 steps{
-sh 'gcc mt-example-0.c -lpthread'
+sh 'gcc multithreading-example1.c -lpthread'
      }
 }
 stage('Test'){
